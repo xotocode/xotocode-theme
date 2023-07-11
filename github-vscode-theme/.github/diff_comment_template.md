3100f5e97d2b37b85f2c524d57a611040cd7ac8f
gregoire cousin <gregoire.cousin@loreal.com>
2023-07-11 21:04:52 +0200

    blast off

--- github-vscode-theme/.github/diff_comment_template.md
+++
@@ -1,5 +0,0 @@
-<details>
-  <summary><strong>Colors changed</strong></summary>
-
-  <!-- diff --><!-- /diff -->
-</details>
---
+++ src/dark-colors.js
@@ -0,0 +1,50 @@
+module.exports = {
+  '--xotonight-desaturated-gray': '#7f85a3',
+  '--xotonight-dark-blue': '#3e68d7',
+  '--xotonight-blue': '#82aaff',
+  '--xotonight-sky-blue': '#65bcff',
+  '--xotonight-cyan': '#86e1fc',
+  '--xotonight-red': '#ff757f',
+  '--xotonight-dark-red': '#ff5370',
+  '--xotonight-light-red': '#ff98a4',
+  '--xotonight-yellow': '#ffc777',
+  '--xotonight-orange': '#ff966c',
+  '--xotonight-dark-orange': '#fc7b7b',
+  '--xotonight-teal': '#4fd6be',
+  '--xotonight-green': '#c3e88d',
+  '--xotonight-purple': '#c099ff',
+  '--xotonight-pink': '#fca7ea',
+  '--xotonight-indigo': '#7a88cf',
+  '--xotonight-bright-cyan': '#b4f9f8',
+  '--xotonight-gray-10-alt': '#bcc4d6',
+  '--xotonight-gray-10': '#c8d3f5',
+  '--xotonight-gray-9': '#b4c2f0',
+  '--xotonight-gray-8': '#a9b8e8',
+  '--xotonight-gray-7': '#828bb8',
+  '--xotonight-gray-6': '#444a73',
+  '--xotonight-gray-5': '#2f334d',
+  '--xotonight-gray-4': '#222436',
+  '--xotonight-gray-3': '#1e2030',
+  '--xotonight-gray-2': '#191a2a',
+  '--xotonight-gray-1': '#131421'
+};
+
+// OTHERS THAT DONT : 
+
+// #00000022
+// #b2dfff
+// #1b1d2c
+// #000000aa
+// #ffffff
+// #00000066
+// #c53b53
+// #446bbb
+// #ad7c43
+// #383e5c
+// #292e46
+// #000000
+// #00000033
+// #00000030
+// #77e0c6dd
+// #777fabaa
+// #00000050
